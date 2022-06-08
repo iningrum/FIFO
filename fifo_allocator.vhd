4f973d0ee79c3efa2a689f2bdd615a99bad01034
Dorin Ingrum <dorin_ingrum@outlook.com>
2022-06-09 01:01:42 +0200

    ogólny kod - nieprzetestowany, niekompilowany

--- fifo_allocator.vhd
+++ fifo_allocator.vhd
@@ -3,10 +3,7 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 -- ENTITY
 entity fifo is
-end entity;
-architecture behaviour of fifo is
-	component FIFO_allocator
-		PORT(
+	PORT(
 			-- inputs 1B
 			PUSH : in std_logic;
 			POP : in std_logic;
@@ -21,113 +18,84 @@ architecture behaviour of fifo is
 			INPUT : in std_logic_vector(7 downto 0);
 			OUTPUT : out std_logic_vector(7 downto 0)
 		);
+end entity;
+architecture bechaviour of fifo is
+	component fifo_allocator
+		port(
+			PUSH : in std_logic;
+			POP : in std_logic;
+			INIT : in std_logic;
+			CLK : in std_logic;
+			-- outputs
+			FULL : out std_logic;
+			EMPTY : out std_logic;
+			NOPOP : out std_logic;
+			NOPUSH : out std_logic
+		);
 	end component;
-	-- 1B in
-	signal PUSH : std_logic;
-	signal POP : std_logic;
-	signal INIT : std_logic;
-	signal CLK : std_logic;
-	-- 1B out
-	signal FULL : std_logic;
-	signal EMPTY : std_logic;
-	signal NOPOP : std_logic;
-	signal NOPUSH : std_logic;
-	-- 8B I/O
-	signal INPUT : std_logic_vector(7 downto 0);
-	signal OUTPUT : std_logic_vector(7 downto 0);
-	-- additional signals
-	constant CKperiod : time := 5 ns;
+	component memory
+		port(
+			INPUT : in std_logic_vector(7 downto 0);
+			OUTPUT : out std_logic_vector(31 downto 0)
+		);
+	end component;
+	signal malloc, free : std_logic;
+	signal size : positive := 0;
+	constant capacity : positive := 3;
 	
-begin
-	uut: FIFO_allocator port map (
-		PUSH => PUSH,
-		CLK => CLK,
-		POP => POP,
-		INIT => INIT,
-		FULL => FULL,
-		EMPTY => EMPTY,
-		NOPOP => NOPOP,
-		NOPUSH => NOPUSH,
-		INPUT => INPUT,
-		OUTPUT => OUTPUT
-	);
-
---	INIT - then
---	5 writes and 5 reads - fifth one should be either NOPOP or NOPUSH fourth one should be FULL or EMPTY
--- testing behaviour of control unit
-TEST_CONTROL_UNIT: process
-begin
-	PUSH <= '0';
-	POP <= '0';
-	INIT <= '0';
-	INPUT <= "00000000";
-	-- execution
-	INIT <= '1';
-	-- pushing
-	for i in 0 to 2 loop
-		PUSH <= NOT PUSH;
-		wait until rising_edge(CLK);
-		assert NOPUSH = '0' AND NOPOP = '0' AND FULL = '0' AND EMPTY = '0'
-			report "Invalid allocation"
-			severity failure;
-	end loop;
-	PUSH <= NOT PUSH;
-	wait until rising_edge(CLK);
-	assert NOPUSH = '0' AND NOPOP = '0' AND FULL = '1' AND EMPTY = '0'
-		report "Memory should be full"
-		severity failure;
-	PUSH <= NOT PUSH;
-	wait until rising_edge(CLK);
-	assert NOPUSH = '1' AND NOPOP = '0' AND FULL = '1' AND EMPTY = '0'
-		report "Memory overflow"
-		severity failure;
-	POP <= NOT POP;
-	wait until rising_edge(CLK);
-	assert NOPUSH = '0' AND NOPOP = '0' AND FULL = '0' AND EMPTY = '0'
-		report "NOPUSH shouldn't be in HIGH state as FIFO is no longer full"
-		severity failure;
-	PUSH <= NOT PUSH;
-	wait until rising_edge(CLK);
-	assert NOPUSH = '0' AND NOPOP = '0' AND FULL = '1' AND EMPTY = '0'
-		report "Memory should be full"
-		severity failure;
-	-- popping
-	for i in 0 to 2 loop
-		POP <= NOT POP;
-		wait until rising_edge(CLK);
-		assert NOPUSH = '0' AND NOPOP = '0' AND FULL = '0' AND EMPTY = '0'
-			report "Invalid allocation"
-			severity failure;
-	end loop;
-	POP <= NOT POP;
-	wait until rising_edge(CLK);
-	assert NOPUSH = '0' AND NOPOP = '0' AND FULL = '0' AND EMPTY = '1'
-		report "Memory should be empty"
-		severity failure;
-	POP <= NOT POP;
-	wait until rising_edge(CLK);
-	assert NOPUSH = '0' AND NOPOP = '1' AND FULL = '0' AND EMPTY = '1'
-		report "Memory overflow"
-		severity failure;
-	PUSH <= NOT PUSH;
-	wait until rising_edge(CLK);
-	assert NOPUSH = '0' AND NOPOP = '0' AND FULL = '0' AND EMPTY = '0'
-		report "NOPOP shouldn't be in HIGH state as FIFO is no longer empty"
-		severity failure;
-	POP <= NOT POP;
-	wait until rising_edge(CLK);
-	assert NOPUSH = '0' AND NOPOP = '0' AND FULL = '0' AND EMPTY = '1'
-		report "Memory should be empty"
-		severity failure;
-	end process;
-	
-	CLK_GEN : process
 	begin
-		CLK <= '0';
-		loop
-			wait for CKperiod;
-			CLK <= not CLK;
-		end loop;
-	wait;
-	end process;
-	END;
+		ALLOCATOR: process(CLK, PUSH, POP, free, malloc) is
+		begin
+			if (rising_edge(CLK)) then
+				EMPTY <= '0';
+				FULL <= '0';
+				NOPOP <='0';
+				NOPUSH <='0';
+				if (size=capacity) then
+					FULL <= '1';
+				elsif (size=0) then
+					EMPTY <='1';
+				end if;
+				if (PUSH='1' AND POP='0') then
+					if(FULL='1') then
+						NOPUSH <= '1';
+						return;
+					end if;
+					malloc <= '1';
+					size <= size +1;
+					if(size=capacity) then
+						FULL <= '1';
+					end if;
+					wait for malloc='0';
+				elsif (POP='1' AND PUSH='0') then
+					if (EMPTY='1') then
+						NOPOP <='1';
+						return;
+					end if;
+					free <= '1';
+					size <= size - 1;
+					if (size = 0) then
+						EMPTY<='1';
+					end if;
+					wait for free='0';
+				end if;
+					
+			end if;
+		end process;
+		MEMORY_DISPLAY : process(INPUT, free, malloc) is
+		begin
+			if (malloc/=free) then
+				if (malloc='1') then
+					for i in size*8 to size*8+7  loop
+						OUTPUT(i) <= INPUT(i-size*8);
+					end loop;
+					malloc <= '0';
+				elsif (free='1') then
+					for i in size*8-1 downto size*8+8  loop
+						OUTPUT(i) <= '0';
+					end loop;
+					free <= '0';
+				end if;
+			end if;
+		end process;
+end architecture;
