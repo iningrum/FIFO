5b990f238fe0780b747a73747a173caa6615d24a
Dorin Ingrum <dorin_ingrum@outlook.com>
2022-07-05 22:57:22 +0200

    .

--- fifo_allocator.vhd
+++ fifo_allocator.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity fifo is
 port(
-init,pop,push : IN std_logic;
+reset,pop,push : IN std_logic;
 clk: in std_logic;
 nopop,nopush,f_ll,e_pty : OUT std_logic);
 end fifo;
@@ -14,73 +14,53 @@ architecture mealymodel of fifo is
 	signal size : natural;
 	constant capacity : natural := 3;
 	begin
-		process(init, clk) begin
-			if init='1' then  
-				-- proper code
-				if rising_edge(CLK)  then
-					case current_state is
-						when EMPTY =>
-							if push='1' and pop='0' then
-								next_state <= UNDEFINED;
-								size <= size+1;
-							end if;
-						when UNDEFINED =>
-							if push='1' and pop='0' and size=capacity-1 then
-								next_state <= FULL;
-							elsif push='0' and pop='1' and size=1 then
-								next_state <= EMPTY;	
-							end if;
-						when FULL =>
-							if push='0' and pop='1' then
-								next_state <= UNDEFINED;
-								size <= size-1;
-							end if;
-						end case;
-						------------------------
-					current_state <= next_state;
-					case current_state is
-						when EMPTY => -- Empty stack
-						report "STATE IS EMPTY | "& integer'image(size) severity warning;
-							if push='1' and pop='0' then
-								nopop <= '0';
-							elsif push='0' and pop='1' then
-								nopop <= '1';
-							elsif push='0' and pop='0' then
-								nopop <='0';
-							end if;
-							nopush<= '0';
-							f_ll <= '0';
-							e_pty <= '1';
-						when UNDEFINED => -- Neither full nor empty
-						report "STATE IS UNDEFINED | "& integer'image(size) severity warning;
-							e_pty<='0';
-							f_ll<='1';
-							if push='1' and pop='0' and size<=capacity then
-								size <= size + 1;
-							elsif push='0' and pop='1' and size/=0 then
-								size <= size - 1;
-							end if;
-						when FULL => -- Full stack
-						report "STATE IS FULL | "& integer'image(size) severity warning;
-							if push='1' and pop='0' then
-								nopush <= '1';
-							elsif push='0' and pop='1' then
-								nopush<='0';
-							end if;
+		StateRegister: PROCESS (clk, reset)
+  BEGIN
+    IF (reset = '1') THEN
+      current_state <= EMPTY;
+    ELSIF rising_edge(clk) THEN
+      current_state <= next_state;
+    END IF;
+  END PROCESS;
 
-						end case;
-					end if;
-			elsif init='0' and rising_edge(clk) then
-				report "----RESET-----" severity warning;
-				-- reset
-				next_state <= EMPTY;
-				current_state <= EMPTY;
-				size <= 0;
-				e_pty <= '1';
-				f_ll <= '0';
-				nopop<='0';
-				nopush<='0';
+  CombLogic: PROCESS (current_state, push, pop)
+  BEGIN
+    case current_state is
+		when EMPTY =>
+			e_pty <= '1';
+			if (push='1' and push xor pop) THEN
+				next_state <= UNDEFINED;
+				size <= size +1;
+			elsif (pop='1' and push xor pop) THEN
+				nopop <= '1';
+			else
+				nopop <= '0';
 			end if;
-		end process;
+		when UNDEFINED =>
+			e_pty <= '0';
+			f_ll <= '0';
+			if (push='1' and push xor pop) then
+				if size=capacity-1 then
+					next_state <= FULL;
+				end if;
+				size <= size +1;
+			elsif (pop='1' and push xor pop) then
+				if size=1 then
+					next_state <= EMPTY;
+				end if;
+				size <= size -1;
+			end if;
+		when FULL =>
+			f_ll <= '1';
+			if (push='1' and push xor pop) THEN
+				nopush <= '1';
+			elsif (pop='1' and push xor pop) THEN
+				size <= size - 1;
+				next_state <= UNDEFINED;
+			else
+				nopop <= '0';
+			end if;
+		end case;
+  END PROCESS;
 
 end mealymodel;
